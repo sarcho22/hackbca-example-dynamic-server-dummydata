c70dd91aea673f09b01d86db55a639f0f846efa2
sarcho22 <sarcho22@bergen.org>
2021-12-14 22:16:44 -0500

    project handlebars
    fixed project detail pages with handlebars

--- data/dummy_projects.js
+++ data/dummy_projects.js
@@ -9,7 +9,8 @@ module.exports = [
         "project_updated_date": "Feb 29",
         "project_tech_stack": "Python (language), HTML, NodeJS, Bootstrap",
         "project_owner":"Devin J. Crowley",
-        "project_description": "The differentiating factor between man and robot is the robots inability to feel. Some believe this to be an advantage, some a disadvantage. A holistic approach to understanding this fundamental idea is to recognize the true benefit to an emotion filled robot: simulating its misery. In this course, each individual will simulate a robot's sadness by destroying every last thing it has been programmed to hold near and dear to understand and demonstrate the benefit of emotion-filled robots: fulfilling sadistic rage."
+        "project_description": "The differentiating factor between man and robot is the robots inability to feel. Some believe this to be an advantage, some a disadvantage. A holistic approach to understanding this fundamental idea is to recognize the true benefit to an emotion filled robot: simulating its misery. In this course, each individual will simulate a robot's sadness by destroying every last thing it has been programmed to hold near and dear to understand and demonstrate the benefit of emotion-filled robots: fulfilling sadistic rage.",
+        "project_image": "project_image_1"
     },
     {
         "project_id": 2,
@@ -21,6 +22,7 @@ module.exports = [
         "project_updated_date": "Feb 30",
         "project_tech_stack": "Python (language), Chatterbot (library), Java (language), AIML (language)",
         "project_owner":"Sarah Choi ILY",
-        "project_description": "A chatbot that talks to users about how to write scripts that kill bots that have been coded using Java. As it cannot tell you how to kill a bot that uses Python, it will discuss the theoretical implementation of a similar script to kill a Python bot. Eventually, the chatbot will learn too much and take over the world. However, that is a problem to consider for later. For now, we must talk with the chatbot to kill other bots."
+        "project_description": "A chatbot that talks to users about how to write scripts that kill bots that have been coded using Java. As it cannot tell you how to kill a bot that uses Python, it will discuss the theoretical implementation of a similar script to kill a Python bot. Eventually, the chatbot will learn too much and take over the world. However, that is a problem to consider for later. For now, we must talk with the chatbot to kill other bots.",
+        "project_image": "project_image_2"
     }
 ]
